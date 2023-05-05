75b8d71b775b091a97eb8cf09583c01940b17bb0
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-05 06:29:38 +0530

    feat: added service for post

--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/shared/service/post/post.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
 
+  constructor(
+    private postService: PostService,
+  ) {
+    
+  }
+  ngOnInit(): void {
+    this.postService.getAll()
+  }
 }
