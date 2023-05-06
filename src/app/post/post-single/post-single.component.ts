dc89bc3e0ea62753f6b3f4867f8ad8c5bb3455c5
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-06 17:16:51 +0530

    feat: post single detial view

--- src/app/post/post-single/post-single.component.ts
+++ src/app/post/post-single/post-single.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from 'src/app/shared/modal/modal';
+import { PostService } from 'src/app/shared/service/post/post.service';
 
 @Component({
   selector: 'app-post-single',
   templateUrl: './post-single.component.html',
   styleUrls: ['./post-single.component.scss']
 })
-export class PostSingleComponent {
-
+export class PostSingleComponent implements OnInit {
+  id!: string | undefined;
+  singlePostDetails!: Post;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private postService: PostService,
+  ) { }
+  ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.postService.entities$.subscribe((res: any) => {
+      this.singlePostDetails = res.find((list: Post) => list.id == this.id)
+    });
+  }
 }
--- src/app/store/entity.metadata.ts
+++ src/app/store/entity.metadata.ts
@@ -1,7 +1,11 @@
 import { EntityDataModuleConfig, EntityMetadataMap } from "@ngrx/data";
 export const POST_ENTITY_NAME = 'Post'
 const entityMetadata: EntityMetadataMap = {
-    [POST_ENTITY_NAME]: {}
+    [POST_ENTITY_NAME]: {
+        entityDispatcherOptions: {
+            optimisticUpdate: true
+        }
+    }
 }
 
 export const entityConfig: EntityDataModuleConfig = {
